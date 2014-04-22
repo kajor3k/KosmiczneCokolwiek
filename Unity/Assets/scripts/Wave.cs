b6dfcd475f67e63aa052c22c83ff43a8a6536114
kajor3k <kajor3k@gmail.com>
2014-04-22 12:50:59 +0200

    message tak bardzo

---
+++ Unity/Assets/scripts/Wave.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wave : MonoBehaviour {
+	float timer = 0.0f;
+	int EnemyAmount=0;
+
+	public Transform EnemySpawnerTop;
+	public Transform EnemySpawnerDown;
+	public Transform EnemySpawnerLeft;
+	public Transform EnemySpawnerRight;
+
+	public GameObject EnemyMissile;
+	// Use this for initialization
+	void Start () {
+
+
+	
+	}
+
+	void EnemySpawnTop()
+	{
+				GameObject.Instantiate (EnemyMissile, EnemySpawnerTop.position, EnemySpawnerTop.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+			
+		if (timer >= 20.0f)
+		{
+
+			EnemyAmount++;
+			timer = 0.0f;
+		}
+		timer += Time.deltaTime;
+	
+	}
+}
---
+++ Unity/Assets/scripts/WaveTop.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wave : MonoBehaviour {
+	float timer = 0.0f;
+	int EnemyAmount=0;
+	float reloadingTime = 1f;
+	bool canShoot = true;
+
+	public Transform EnemySpawnerTop;
+
+
+	public GameObject EnemyMissile;
+	// Use this for initialization
+	void Start () {
+
+
+	
+	}
+
+	void EnemySpawnTop()
+	{
+				GameObject.Instantiate (EnemyMissile, EnemySpawnerTop.position, EnemySpawnerTop.rotation);
+			canShoot = false;
+			StartCoroutine(Reload(reloadingTime));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+			
+		if (timer >= 20.0f)
+		{
+			if(EnemyAmount!=0)
+			{
+				EnemySpawnTop();
+				count--;
+
+			}
+
+			EnemyAmount++;
+			timer = 0.0f;
+		}
+		timer += Time.deltaTime;
+	
+	}
+}
